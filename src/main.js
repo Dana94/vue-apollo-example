afe7c9c2841e8dd0b08e3c3dad1452cf70ce9c7c
Dana <dana.ottaviani@gmail.com>
2020-06-30 10:47:33 -0400

    Import and install vue-apollo

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueApollo from 'vue-apollo';
+
+import ApolloClient from 'apollo-boost';
+
+Vue.use(VueApollo)
 
 Vue.config.productionTip = false
 
+const apolloClient = new ApolloClient({
+  // You should use an absolute URL here
+  uri: 'https://graphqlpokemon.favware.tech/'
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
 new Vue({
+  apolloProvider,
   render: h => h(App),
 }).$mount('#app')
