31caf50dc9cc3e6493b19dedbdf2dbffc3d40229
Dana <dana.ottaviani@gmail.com>
2020-07-01 10:17:10 -0400

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,76 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <p>{{anime}}</p> -->
+    <p>
+      <img :src="dragonite.sprite" />
+    </p>
+    <p>{{bulbasaur}}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import gql from 'graphql-tag'; // need
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      dragonite: [],
+      bulbasaur: []
+    }
+  },
+  apollo: {
+    // anime: {
+    //   query: gql`query getMedia ($id: Int) {
+    //     Media (id: $id, type: ANIME) {
+    //       characters {
+    //         nodes {
+    //           name {
+    //             first
+    //             last
+    //           }
+    //         }
+    //       }
+    //       id
+    //       title {
+    //         english
+    //       }
+    //     }
+    //   }`,
+    //   loadingKey: "Tags loading...",
+    //   update: data => data.Media,
+    //   variables() {
+    //     return {
+    //       id: 8888 // variable example + how I did it with quotes database
+    //     }
+    //   }
+    // },
+    // set paramters example
+    dragonite: {
+      query: gql`query{
+        getPokemonDetailsByName(pokemon: dragonite reverse: true take: 1) {
+          sprite
+        }
+      }`,
+      loadingKey: "Loading dragonite...",
+      update: data => data.getPokemonDetailsByName
+    },
+    // using parameters with variables example
+    bulbasaur: {
+      query: gql`query getPokemon($pokemon: String!){
+        getDexEntries(pokemon: $pokemon) {
+          species
+          evos
+        }
+      }`,
+      loadingKey: "Loading bulbasaur...",
+      update: data => data.getDexEntries,
+      variables() {
+        return {
+          pokemon: "bulbasaur"
+        }
+      }
+    }
   }
 }
 </script>
